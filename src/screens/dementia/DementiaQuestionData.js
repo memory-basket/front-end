509aae629d78898676aeb2d585e07b9b306764f8
yunhee1 <alkrg007@naver.com>
2023-06-08 05:14:22 +0900

    #20 Feat: Add Dementia questions data

---
+++ src/screens/dementia/DementiaQuestionData.js
@@ -0,0 +1,200 @@
+const dementia_questions = [
+  {
+    id: 1,
+    question: "자신의 기억력에 문제가 있다고 생각하십니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "자신의 기억력이 10년 전보다 나빠졌다고 생각하십니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "자신의 기억력이 같은 또래의 다른 사람들에 비해 나쁘다고 생각하십니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      { type: 1, answer: "아니오" },
+    ],
+  },
+  {
+    id: 4,
+    question: "기억력 저하로 인해 일상생활에 불편을 느끼십니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "최근에 일어난 일을 기억하는 것이 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "며칠 전에 나눈 대화 내용을 기억하기 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "며칠 전에 한 약속을 기억하기 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "친한 사람의 이름을 기억하기 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "물건 둔 곳을 기억하기 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "이전에 비해 물건을 자주 잃어버립니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "집 근처에서 길을 잃은 적이 있습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 12,
+    question:
+      "가게에서 2-3가지 물건을 사려고 할 때 물건 이름을 기억하기 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "가스불이나 전기불 끄는 것을 기억하기 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+  {
+    id: 14,
+    question:
+      "자주 사용하는 전화번호(자신 혹은 자녀의 집)를 기억하기 어렵습니까?",
+    answers: [
+      {
+        type: 0,
+        answer: "예",
+      },
+      {
+        type: 1,
+        answer: "아니오",
+      },
+    ],
+  },
+];
+
+export default questions;
