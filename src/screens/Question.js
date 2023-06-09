19994c101a4f9eb998a0c3f53df0bc102f4b7724
yunhee1 <alkrg007@naver.com>
2023-06-09 12:29:45 +0900

    #14 Feat: 홈화면 배경 수정중

--- src/screens/Question.js
+++ src/screens/Question.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 //import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from "react-native";
 //import Button from '../components/Button';
-import styled from 'styled-components/native';
-
+import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
-  //background-color: #fff;
+  background-color: #fff;
   align-items: center;
   justify-content: center;
 `;
@@ -18,10 +17,23 @@ const StyledText = styled.Text`
   margin: 20px;
 `;
 
+const MainBody = styled.View`
+  flex: 3;
+  width: 100%;
+  height: 200px;
+  // background-color: blue;
+  // background-image: url("../../assets/flower1_img.png");
+  // background-size: 100%;
+`;
 const Question = ({ navigation }) => {
   return (
     <Container>
+      <MainBody>
+        {/* <Image source={require("../../assets/flower1_img.png")} /> */}
+        <Text>홍길동 보호자님 오늘을 같이 기억해요!</Text>
+      </MainBody>
       <StyledText>오늘의 질문 페이지입니다!</StyledText>
+
       {/* <Button
         title="오늘의 질문"
         //onPress={() => navigation.navigate('EnterName')}
