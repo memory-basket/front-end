93d4acf7003c02ee24a8c6751199ff5847b12526
minjun06 <35772631+minjun06@users.noreply.github.com>
2023-06-14 02:41:07 +0900

    merge

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,14 +1,39 @@
-import { NavigationContainer } from "@react-navigation/native";
-import StackNav from "./Stack";
-//import TabNav from "./Tab";
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNav from './Stack';
+import TabNav from './Tab';
 //import Auth from "./Auth";
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <StackNav />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <StackNav>
+    //     <Stack.screen name="Tab" component={TabNav} />
+    //   </StackNav>
+    // </NavigationContainer>
+
+    <Stack.Navigator>
+      <Stack.Screen name="Tab" component={TabNav} />
+    </Stack.Navigator>
   );
 };
+
+// const TabNavigation = () => {
+//     return (
+//       <NavigationContainer>
+//         <TabNav/>
+//       </NavigationContainer>
+//     );
+//   };
+
+//   const AppNavigation = () => {
+//     return (
+//       <NavigationContainer>
+//         <StackNav>
+//             <StackNav.Screen name="Stack" component={TabNavigation}/>
+//         </StackNav>
+//       </NavigationContainer>
+//     );
+//   };
 
 export default Navigation;
